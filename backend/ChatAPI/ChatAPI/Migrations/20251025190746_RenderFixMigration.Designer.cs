fb05c8dd4f56c4cde543419a5567b272429a242e
Okan Alat <86848048+okanalatt@users.noreply.github.com>
2025-10-25 22:08:12 +0300

    Migrations
    Add RenderFixMigration

--- backend/ChatAPI/ChatAPI/Migrations/20251025190746_RenderFixMigration.Designer.cs
+++ backend/ChatAPI/ChatAPI/Migrations/20251025190746_RenderFixMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ChatAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChatAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251025190746_RenderFixMigration")]
+    partial class RenderFixMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
---
+++ backend/ChatAPI/ChatAPI/Migrations/20251025190746_RenderFixMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenderFixMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SentimentResponses",
+                columns: table => new
+                {
+                    label = table.Column<string>(type: "TEXT", nullable: false),
+                    score = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SentimentResponses");
+        }
+    }
+}
